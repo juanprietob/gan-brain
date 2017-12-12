35c0021b835f7e91e67510a8b0d37d5bc56875d3
Juan Carlos Prieto <juanprietob@gmail.com>
2017-12-12 13:36:26 -0500

    ENH: Add sampling for image using different neighborhood size
    Possibility to have neighborhoods for sampling with different size and in addition, sample a second 'label' image when sampling the primary input image, i.e., the input and the label are in the same space and have the same dimensions when are written to the output

--- src/ganutil/sampleImage.cxx
+++ src/ganutil/sampleImage.cxx
@@ -14,14 +14,14 @@
 
 using namespace std;
 
-typedef unsigned short PixelType;
+typedef double PixelType;
 static const int Dimension = 3;
 
 typedef itk::Image<PixelType, Dimension> InputImageType;
 typedef InputImageType::IndexType InputImageIndexType;
 typedef itk::ImageFileReader<InputImageType> InputImageFileReaderType;
 
-typedef unsigned short VectorImagePixelType;
+typedef double VectorImagePixelType;
 typedef itk::VectorImage<VectorImagePixelType, Dimension> VectorImageType;  
 typedef VectorImageType::IndexType VectorImageIndexType;
 typedef itk::ComposeImageFilter< InputImageType, VectorImageType> ComposeImageFilterType;
@@ -72,26 +72,52 @@ int main (int argc, char * argv[]){
 		maskimage = readimage->GetOutput();	
 	}
 
+	VectorImageType::Pointer vectorlabelimage = 0;
+
+	if(vectorLabelImageFilename.size() > 0){
+		ComposeImageFilterType::Pointer composeImageFilter = ComposeImageFilterType::New();
+
+		for(int i = 0; i < vectorLabelImageFilename.size(); i++){
+			cout<<"Reading label image:"<<vectorLabelImageFilename[i]<<endl;
+			InputImageFileReaderType::Pointer readimage = InputImageFileReaderType::New();
+			readimage->SetFileName(vectorLabelImageFilename[i]);
+			readimage->Update();
+
+			composeImageFilter->SetInput(i, readimage->GetOutput());
+		}
+
+		composeImageFilter->Update();
+		
+		vectorlabelimage = composeImageFilter->GetOutput();
+	}
 
 	VectorImageType::SizeType outputsize;
-	outputsize[0] = neighborhood*2 + 1;
-	outputsize[1] = neighborhood*2 + 1;
-	outputsize[2] = neighborhood*2 + 1;
+	outputsize[0] = neighborhood[0]*2 + 1;
+	outputsize[1] = neighborhood[1]*2 + 1;
+	outputsize[2] = neighborhood[2]*2 + 1;
 
 	char buf[50];	
 
 	VectorImageType::RegionType region = vectorimage->GetLargestPossibleRegion();
 
+	//cout<<region<<endl;
+
 	for(int i = 0; i < Dimension; i++){
-		region.SetIndex(i, region.GetIndex(i) + neighborhood);
-		region.SetSize(i, region.GetSize(i) - 2*neighborhood);
+
+		if(region.GetSize(i) < 2*neighborhood[i]){
+			cerr<<"The size of the extracted region is larger than the image region!"<<2*neighborhood[i]<<">"<<region.GetSize(i)<<endl;
+			return EXIT_FAILURE;
+		}
+
+		region.SetIndex(i, region.GetIndex(i) + neighborhood[i]);
+		region.SetSize(i, region.GetSize(i) - 2*neighborhood[i]);
 	}
 
-	cout<<region<<endl;
+	//cout<<region<<endl;
 	
 
 	InputRandomIteratorType randomit(vectorimage, region);
-	randomit.SetNumberOfSamples(vectorimage->GetLargestPossibleRegion().GetNumberOfPixels() - (pow(neighborhood,3)));
+	randomit.SetNumberOfSamples(vectorimage->GetLargestPossibleRegion().GetNumberOfPixels());
 	randomit.GoToBegin();
 
 
@@ -102,9 +128,9 @@ int main (int argc, char * argv[]){
 		bool extractsample = true;
 
 		VectorImageIndexType n_index;
-		n_index[0] = index[0] - neighborhood;
-		n_index[1] = index[1] - neighborhood;
-		n_index[2] = index[2] - neighborhood;
+		n_index[0] = index[0] - neighborhood[0];
+		n_index[1] = index[1] - neighborhood[1];
+		n_index[2] = index[2] - neighborhood[2];
 
 		if(maskimage){
 			extractsample = maskimage->GetPixel(index) != 0;
@@ -120,25 +146,42 @@ int main (int argc, char * argv[]){
 			extractimage->SetExtractionRegion(outputRegion);
 			extractimage->Update();
 
+			string outprefix;
 			string outfilename;
 
 			if(prefix.compare("")!=0){
 				sprintf(buf, "%d-%d-%d", n_index[0], n_index[1], n_index[2]);
-				outfilename = outputImageDirectory + "/" + prefix + string(buf) + ".nrrd";
+				outprefix = outputImageDirectory + "/" + prefix + string(buf);
+				outfilename = outprefix + ".nrrd";
 			}else{
 				char *uuid = new char[100];	
 				uuid_t id;
 				uuid_generate(id);
 			  	uuid_unparse(id, uuid);
-			  	outfilename = outputImageDirectory + "/" + string(uuid) + ".nrrd";
+			  	outprefix = outputImageDirectory + "/" + string(uuid);
+			  	outfilename = outprefix + ".nrrd";
 			}
-
-			
 
 			cout<<"Writing: "<<outfilename<<endl;
 			writer->SetFileName(outfilename);
 			writer->SetInput(extractimage->GetOutput());
 			writer->Update();
+
+			if(vectorlabelimage){
+				ExtractImageFilterType::Pointer extractimage = ExtractImageFilterType::New();
+				extractimage->SetInput(vectorlabelimage);			
+				extractimage->SetExtractionRegion(outputRegion);
+				extractimage->Update();
+
+				string outfilename = outprefix + "_label.nrrd";
+
+				cout<<"Writing label: "<<outfilename<<endl;
+				writer->SetFileName(outfilename);
+				writer->SetInput(extractimage->GetOutput());
+				writer->Update();
+
+			}
+
 			numSamples--;
 		}
 
